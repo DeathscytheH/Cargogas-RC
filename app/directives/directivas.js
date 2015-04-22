aba5f495dadf04ed2cee1f2ee9fc8ce3c9171cb8
DeathscytheH <alejandro.deathscythe@gmail.com>
2015-04-22 01:42:53 -0500

    Estructura del proyecto lista.

---
+++ app/directives/directivas.js
@@ -0,0 +1,18 @@
+app.directive('inicio', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/inicio.html'
+    };
+});
+app.directive('banner', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/banner.html'
+    };
+});
+app.directive('carrucel', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/carrucel.html'
+    };
+});
