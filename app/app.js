ab388749496a025462a07006a2cf88b28a58823e
DeathscytheH <alejandro.deathscythe@gmail.com>
2015-06-17 19:24:57 -0500

    Imagenes eliminadas.
    Eliminadas imágenes para reducir tamaño.

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('CargogasApp', ['ngRoute', 'ui.bootstrap', 'ngMap']);
+var app = angular.module('CargogasApp', ['ngRoute', 'ui.bootstrap']);
 /**/
 app.config(function ($routeProvider) {
   $routeProvider
